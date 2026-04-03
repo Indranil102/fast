b01a66c855cbef77ed8bdf552fd0c16d79a4782b
Indranil102e <indranilsamanta2003@gmail.com>
2026-04-03 13:04:18 +0530

    Add product service and API endpoint to retrieve products

--- fastapi-ecom/app/app.py
+++ fastapi-ecom/app/app.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI, HTTPException
-
+from app.service.products import get_all_products
 app= FastAPI()
 
 @app.get("/")
 def home():
     return {"message":"hii"}
 
+@app.get("/products")
+def get_products():
+    return get_all_products()
--- fastapi-ecom/app/service/products.py
+++ fastapi-ecom/app/service/products.py
@@ -2,12 +2,15 @@ import json
 from pathlib import Path
 from typing import List,Dict 
 
-DATA_FILE= Path("..","data","products.json")
+DATA_FILE= Path(__file__).parent.parent / "data" / "products.json"
 
 
 # here we load the product form the json file and return it as a list of dictionaries
 def load_products()-> List[Dict]:
     if not DATA_FILE.exists():
         return []
-    with open(DATA_FILE,"r","utf-8") as file:
+    with open(DATA_FILE,"r",encoding="utf-8") as file:
         return json.load(file)
+    
+def get_all_products()->List[Dict]:
+    return load_products()
