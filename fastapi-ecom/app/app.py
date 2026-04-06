c13285ddda9a8615e47f8848f2ac181c760bdfbc
Indranil102e <indranilsamanta2003@gmail.com>
2026-04-06 17:53:53 +0530

    Refactor get_products endpoint to include sorting functionality by price and improve name query validation

--- fastapi-ecom/app/app.py
+++ fastapi-ecom/app/app.py
@@ -11,15 +11,34 @@ def home():
 #     return get_all_products()
 
 @app.get("/products")
-def get_products(name:str=Query(default=None, min_length=3, max_length=50, description="Search by product name (case insensitive)")
+#hwere i can sort the thing with name and price 
+def get_products(
+    name:str=Query(
+        default=None, 
+        min_length=1, 
+        max_length=50, 
+        description="Search by product name (case insensitive)"
+        ),
+    sort_price:bool=Query(
+        default=False,
+        description="Sort products by price (ascending)"
+    ),
+    order: str=Query(
+        default="asc",
+        description="Sort order: 'asc' for ascending, 'desc' for descending",
+    )
                  ):
     products= get_all_products()
     if name:
         needle= name.strip().lower().strip('"')
         products= [p for p in products if needle in p.get("name","").lower()]
         
-        if not products:
-            raise HTTPException(status_code=404, detail="No products found matching the search criteria")
+    if not products:
+        raise HTTPException(status_code=404, detail="No products found matching the search criteria")
+            
+    if sort_price:
+        reverse= order.lower() == "desc"
+        products=sorted(products,key=lambda x: x.get("price",0), reverse=reverse)
     
     total = len(products)
     return {"total": total, "products": products}
